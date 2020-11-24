31839945d662c1d7c3b3a2166537c796a44a06a9
LakunCindy <43467524+LakunCindy@users.noreply.github.com>
2020-11-24 17:23:56 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,18 +1,18 @@
 package com.example;
 
- /*
+ /**
   * This is a class.
   */
 public class Greeter {
 
-  /*
+  /**
   * This is a constructor.
   */
   public Greeter() {
 
   }
   
-  /*
+  /**
   * @param someone name of a person
   * @return greeting string
   */
